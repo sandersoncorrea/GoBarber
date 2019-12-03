f054ded64267d7402a6277529bb6eafa4fb6e4c1
sandersoncorrea <correasanderson@gmail.com>
2019-12-03 10:09:49 -0300

    #11 - Cadastro de usuario, controller

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/constrollers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Sanderson',
-        email: 'correasanderson@gmail.com',
-        password_hash: 'qwerty123',
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
