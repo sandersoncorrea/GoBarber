62d3ba0cb4bb6f1eae67ab33c0e7af636539d50e
sandersoncorrea <correasanderson@gmail.com>
2019-12-03 17:09:36 -0300

    #3#05 - agendando serviços

---
+++ src/app/constrollers/AppointmentsController.js
@@ -0,0 +1,35 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentsController {
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            provider_id: Yup.number().required(),
+            date: Yup.date().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Inválido' });
+        }
+
+        const { provider_id, date } = req.body;
+        const isProvider = await User.findOne({
+            where: { id: provider_id, provider: true },
+        });
+        if (!isProvider) {
+            return res.status(401).json({
+                error: 'Só é permitido criar agendamentos com profissionais',
+            });
+        }
+
+        const appointment = await Appointment.create({
+            user_id: req.userId,
+            provider_id,
+            date,
+        });
+        return res.json(appointment);
+    }
+}
+
+export default new AppointmentsController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/constrollers/FileController';
 import ProvidersController from './app/constrollers/ProvidersController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentsController from './app/constrollers/AppointmentsController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,4 +20,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProvidersController.index);
+
+routes.post('/appointments', AppointmentsController.store);
 export default routes;
