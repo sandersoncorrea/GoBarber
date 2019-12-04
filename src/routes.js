32a2fc21659ba470caf42ac65349130ed24a8f53
sandersoncorrea <correasanderson@gmail.com>
2019-12-04 08:30:14 -0300

    #3#09 - Listar agenda do dia

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import UserController from './app/constrollers/UserController';
 import SessionController from './app/constrollers/SessionController';
 import FileController from './app/constrollers/FileController';
 import ProvidersController from './app/constrollers/ProvidersController';
-
+import SchedulesController from './app/constrollers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentsController from './app/constrollers/AppointmentsController';
 
@@ -23,4 +23,6 @@ routes.get('/providers', ProvidersController.index);
 
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+
+routes.get('/schedules', SchedulesController.index);
 export default routes;
