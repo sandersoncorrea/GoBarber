09a2235f75a28711f5f94f5447cdd6754b3402da
sandersoncorrea <correasanderson@gmail.com>
2019-12-05 09:00:06 -0300

    monitorando erros na fila

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
     port: '2525',
     secure: false,
     auth: {
-        user: '107e632ec6153e',
-        pass: '64aa2aaddad70f',
+        user: 'ded8fa88372fe4',
+        pass: '8080f8f6e29409',
     },
     default: {
         from: 'Equipe GoBarber <correasanderson@gmail.com>',
